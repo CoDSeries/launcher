5d1a82adf79d0a63b334dc3a5503f45497a73233
Enzo Deshayes <enzo.deshayes10@icloud.com>
2024-03-02 20:34:14 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom';
-import Login from './modules/login/Login';
-import Home from './template/Home';
-import { AuthProvider } from './AuthProvider';
-import MatchMenu from "./template/match/menu/MatchMenu";
 import './App.css';
 import './index.css'
-import MatchType from "./template/match/matchType/MatchType";
-import LaunchMatch from "./template/match/launchMatch/LauncMatch";
+
 
 const App = () => {
     return (
         <Router>
-            <AuthProvider>
-                <Routes>
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/home" element={<Home />} />
-                    <Route path="/match" element={<MatchMenu />} />
-                    <Route path="/match/origins/:matchType" element={<MatchType />} />
-                    <Route path="/launch/:gameMode" element={<LaunchMatch />} />
-                    <Route path="/" element={<Navigate replace to="/login" />} />
-                </Routes>
-            </AuthProvider>
+
         </Router>
     );
 };
--- src/modules/login/Login.jsx
+++ src/modules/login/Login.jsx
@@ -1,63 +1,9 @@
-import React, { useState } from 'react';
-import { useAuth } from "../../AuthProvider";
-import { useNavigate } from 'react-router-dom';
-
 const Login = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const auth = useAuth();
-    const navigate = useNavigate();
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-
-        const response = await fetch('http://localhost:5001/api/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
-
-        try {
-            const data = await response.json();
-            if (response.status === 200) {
-                auth.login({ ...data.user, token: data.token });
-                navigate('/home');
-            } else {
-                console.error('Login failed:', data.message);
-            }
-        } catch (error) {
-            console.error('Error parsing JSON response:', error);
-        }
-    };
-
     return (
-        <div>
-            <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
-                    <input
-                        type="email"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Password:</label>
-                    <input
-                        type="password"
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                        required
-                    />
-                </div>
-                <button type="submit">Login</button>
-            </form>
-        </div>
-    );
-};
+        <>
 
-export default Login;
+        </>
+    )
+}
+
+export default Login
